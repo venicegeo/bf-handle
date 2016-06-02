29cf37bcd1e6b78ed7d498d23a43f3a4d44cffdf
bbarden <benjamin.m.barden@gmail.com>
2016-06-02 11:49:02 -0400

    Attemptign to add CORS support.

--- main.go
+++ main.go
@@ -38,6 +38,18 @@ Various TODOs:
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		}
+		// Stop here if its Preflighted OPTIONS request
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		r.ParseForm()
 		switch r.URL.Path {
 		case "/":
