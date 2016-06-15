0268a01897c470fe654cb512304423e4d72062f7
bbarden <benjamin.m.barden@gmail.com>
2016-06-15 14:04:03 -0400

    Added general case of feature-level metadata handling

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func proc (w http.ResponseWriter, r *http.Request) {
 		PzAddr		string			`json:"pzAddr"`
 		DbAuth		string			`json:"dbAuthToken"`
 	}
-fmt.Println ("start")		
+	fmt.Println ("bf-handle called.")		
 	inpBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintln(w, "Error: ioutil.ReadAll: " + err.Error())
@@ -102,26 +102,40 @@ fmt.Println ("start")
 		inpObj.DbAuth = os.Getenv("BFH_DB_AUTH")
 	}
 
+	fmt.Println ("bf-handle: provisioning begins.")
 	dataIDs, err := provision(&inpObj.MetaJSON, inpObj.DbAuth, inpObj.PzAuth, inpObj.PzAddr, inpObj.Bands)
 	if err != nil{
 		fmt.Fprintln(w, "Error: bf-handle provisioning: " + err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 	}
 
-	fmt.Println ("running Algo")
-
+	fmt.Println ("bf-handle: running Algo")
 	resDataID, err := runAlgo(inpObj.AlgoType, inpObj.AlgoURL, inpObj.PzAuth, dataIDs)
 	if err != nil{
 		fmt.Fprintln(w, "Error: algo result: " + err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 	}
 	
-	fmt.Println (`updating Data ( dataId = ` + resDataID + `)`)	
-	err = updateData (resDataID, inpObj.PzAddr, inpObj.PzAuth, &inpObj.MetaJSON)
+	fmt.Println (`bf-handle: getting metadata ( dataId = ` + resDataID + `)`)	
+	attMap, err := getS3Meta (resDataID, inpObj.PzAddr, inpObj.PzAuth, &inpObj.MetaJSON)
 	if err != nil{
-		fmt.Fprintln(w, "Error: bf-handle update data: " + err.Error())
+		fmt.Fprintln(w, "Error: bf-handle get metadata: " + err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
-	}	
+	}
+
+// **** use the following to update S3 metadata for ossim-like algos
+//	err = pzsvc.UpdateFileMeta(resDataID, inpObj.PzAddr, inpObj.PzAuth, attMap)
+//	if err != nil {
+//		fmt.Fprintln(w, "Error: bf-handle update s3 metadata: " + err.Error())
+//		w.WriteHeader(http.StatusInternalServerError)
+//	}
+
+	resDataID, err = addGeoFeatureMeta(resDataID, inpObj.PzAddr, inpObj.PzAuth, attMap)
+	if err != nil {
+		fmt.Fprintln(w, "Error: bf-handle update feature metadata: " + err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 	fmt.Println ("outputting")
 	fmt.Fprintf(w, resDataID)
 }
@@ -141,8 +155,16 @@ func provision(metaDataFeature *geojson.Feature, dbAuth, pzAuth, pzAddr string, 
 		if err != nil {
 			return nil, err
 		}
-		fName := fmt.Sprintf("%s-%s.TIF", fSource, band)	
-		dataID, err := pzsvc.IngestTiffReader(fName, pzAddr, fSource, "", pzAuth, reader, nil)
+		fName := fmt.Sprintf("%s-%s.TIF", fSource, band)
+
+		bSlice, err := ioutil.ReadAll(reader)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: at some point, we might wish to add properties to the TIFF files as we ingest them.
+		// We'd do that by replacing the "nil", below, with an appropriate map[string]string.
+		dataID, err := pzsvc.Ingest(fName, "raster", pzAddr, fSource, "", pzAuth, bSlice, nil)
 		if err != nil {
 			return nil, err
 		}
@@ -211,16 +233,21 @@ func runOssim(algoURL, imgID1, imgID2, authKey string) (string, error) {
 	}
 	
 	return outDataID, nil
+
+// new format:
+// ossim-cli shoreline -i garden_b3.tif, garden_b6.tif  --prop prop1:myprop1 --prop prop2:myprop2 debug.json
+// prop1:myprop1 and prop2:myprop2 are arbitrary key/value pairs for this purpose.  They'll be added to the properties.
+// the filenames (garden_b3.tif and garden_b6.tif) will also be saved in an "input_files" list.  Figure out a way to
+// make that work well.  Talk with Mark about what sort of information would fit well into that space.
+
 }
 
-// updateData modifies the S3 metadata of the given file.  Specifically, it
-// adds information on the image source - what external source it was drawn
-// from, the image ID at that source, the date/time of image collection, and
-// the name of the sensor that did the collecting.
-func updateData(dataID, pzAddr, pzAuth string, feature *geojson.Feature) error {
+// updateS3Meta grabs the existing metadata for a file from the S3 bucket, adds the metadata that
+// can be gleaned from the geojson feature provided by the imag harvester, and returns the result.
+func getS3Meta(dataID, pzAddr, pzAuth string, feature *geojson.Feature) (map[string]string, error) {
 	dataRes, err := pzsvc.GetFileMeta(dataID, pzAddr, pzAuth)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	attMap := make(map[string]string)
@@ -231,15 +258,42 @@ func updateData(dataID, pzAddr, pzAuth string, feature *geojson.Feature) error {
 	attMap["sourceID"] = feature.ID // covers source and ID in that source
 	attMap["dateTimeCollect"] = feature.PropertyString("acquiredDate")
 	attMap["sensorName"] = feature.PropertyString("sensorName")
-	err = pzsvc.UpdateFileMeta(dataID, pzAddr, pzAuth, attMap)
-	if err != nil {
-		return err
-	}
+	attMap["resolution"] = feature.PropertyString("resolution")
 	
-	return nil
-/*
-TODO: still want to pass over SRC_RESOLUTION (float)
-currently don't knwo where to get it.  Talk with Jeff?
-*/
-
+	return attMap, nil
 }
+
+// addGeoFeatureMeta adds metadata to every feature in a given geojson file.  It uses the
+// dataId both to download the geojson file in question from S3  It then iterates through
+// all fo the features in the file and adds the given properties to each, before uploading
+// the file that results and returning the dataId from that upload. 
+func addGeoFeatureMeta(dataID, pzAddr, pzAuth string, props map[string]string) (string, error) {
+	b, err := pzsvc.DownloadBytes(dataID, pzAddr, pzAuth)
+	var obj geojson.FeatureCollection
+	err = json.Unmarshal(b, &obj)
+	if err != nil {
+		return "", err
+	}
+
+	for _, feat := range obj.Features{
+		for pkey, pval := range props{
+			feat.Properties[pkey] = pval
+		}
+	}
+
+	b2, err := json.Marshal(obj)
+	if err != nil {
+		return "", err
+	}
+// TODO: fill in some of those empty quotes.
+	dataID, err = pzsvc.Ingest("", "geojson", pzAddr, "", "", pzAuth, b2, props)
+
+	return dataID, err
+}
+
+
+
+
+
+
+
