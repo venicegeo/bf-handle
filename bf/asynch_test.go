d7bc4856ea99bfbd2fed0bf0bd6a09c2d2b9e570
bpeizer <Peizer@Bens-MacBook-Pro.local>
2016-11-02 11:27:06 -0400

    Refactor for redis

--- bf/asynch_test.go
+++ bf/asynch_test.go
@@ -15,23 +15,10 @@
 package bf
 
 import (
-	//"encoding/json"
-	//	"errors"
-	"fmt"
-	//"io/ioutil"
-	//"log"
-	"net"
-	"net/http"
-	//	"os"
-	//	"strconv"
-	//"strings"
-	//"sync"
-	"testing"
-	"time"
-
-	//"github.com/venicegeo/pzsvc-image-catalog/catalog"
+	"github.com/venicegeo/pzsvc-image-catalog/catalog"
 	"github.com/venicegeo/pzsvc-lib"
-	"gopkg.in/redis.v3"
+	"net/http"
+	"testing"
 )
 
 /*
@@ -62,121 +49,17 @@ Once we have that ugliness black-boxed, figuring out what we want to have come o
 
 */
 
-var mockConnOutpBytes [][]byte
-
-type mockAddr struct{}
-
-func (ma mockAddr) Network() string {
-	return "Network"
-}
-
-func (ma mockAddr) String() string {
-	return "String"
-}
-
-type mockConn struct {
-	readCount *int
-}
-
-var mockConnCount int
-var mockConnInst = mockConn{readCount: &mockConnCount}
-
-/*
-now we need a series of functions that will populate the
-outpBytes (iterating writeCount with each line) based on
-the ints, strings, and errors we want to see in each case.
-
-For that, we first need the byte format objective.
-Digging commences.
-
-In particular, we're interested in what all touches the
-Read port of the net.Conn from teh Dialer in the options.
-
-- all of it (other htan a couple of tests) goes through redis.getDialer in redis.v3/options.go
-- That *only* gets referenced in options.go/newConnPool
-- This, in turn, only sees the light fo day as the return for `(p *ConnPool) dial()`,
-  ...which feeds straight into `(p *ConnPool) NewConn()`
-- In the end, it essentially just gets pumped right back out of the redis.Conn Read and Write functions.
-
-
-*/
-
-func (mCn mockConn) Read(b []byte) (n int, err error) {
-	fmt.Printf("reading: %d of %d.\n", *mCn.readCount, len(mockConnOutpBytes))
-	if *mCn.readCount < len(mockConnOutpBytes) {
-		copy(b, mockConnOutpBytes[*mCn.readCount])
-		*mCn.readCount = *mCn.readCount + 1
-	}
-	return len(b), nil
-}
-
-func (mCn mockConn) Write(b []byte) (n int, err error) {
-	return len(b), nil
-}
-
-func (mCn mockConn) Close() error {
-	return nil
-}
-
-func (mCn mockConn) LocalAddr() net.Addr {
-	return mockAddr{}
-}
-
-func (mCn mockConn) RemoteAddr() net.Addr {
-	return mockAddr{}
-}
-
-func (mCn mockConn) SetDeadline(t time.Time) error {
-	return nil
-}
-
-func (mCn mockConn) SetReadDeadline(t time.Time) error {
-	return nil
-}
-
-func (mCn mockConn) SetWriteDeadline(t time.Time) error {
-	return nil
-}
-
-func MockDialer() (net.Conn, error) {
-	//build correct net.Conn here.
-	return mockConnInst, nil
-}
-
-func makeMockRedisCli(outputs []string) *redis.Client {
-	opt := redis.Options{Dialer: MockDialer}
-	cli := redis.NewClient(&opt)
-	mockConnOutpBytes = make([][]byte, len(outputs), len(outputs))
-	for i, output := range outputs {
-		mockConnOutpBytes[i] = []byte(output)
-	}
-	return cli
-}
-
-func redisConvInt(val int) string {
-	return fmt.Sprintf(":%d\r\n", val)
-}
-func redisConvStatus(val string) string {
-	return fmt.Sprintf("+%s\r\n", val)
-}
-func redisConvString(val string) string {
-	return fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)
-}
-func redisConvErrStr(val string) string {
-	return fmt.Sprintf("-%s\r\n", val)
-}
-
 func TestHandleAsynch(t *testing.T) {
 }
 
 func TestAddAsynchJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	outputs := []string{
-		redisConvErrStr("Error: totally an error."),
-		redisConvStatus("Pending"),
-		redisConvInt(5),
-		redisConvStatus("Pending")}
-	redisCli = makeMockRedisCli(outputs)
+		catalog.RedisConvErrStr("Error: totally an error."),
+		catalog.RedisConvStatus("Pending"),
+		catalog.RedisConvInt(5),
+		catalog.RedisConvStatus("Pending")}
+	redisCli = catalog.MakeMockRedisCli(outputs)
 	taskChan = make(chan string)
 	w, outstr, outInt := pzsvc.GetMockResponseWriter()
 	r := http.Request{}
@@ -197,12 +80,12 @@ func TestAddAsynchJob(t *testing.T) {
 }
 
 func TestGetAsynchStatus(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	outputs := []string{
-		redisConvErrStr("Error: totally an error."),
-		redisConvStatus("Syntax error"),
-		redisConvStatus("Pending")}
-	redisCli = makeMockRedisCli(outputs)
+		catalog.RedisConvErrStr("Error: totally an error."),
+		catalog.RedisConvStatus("Syntax error"),
+		catalog.RedisConvStatus("Pending")}
+	redisCli = catalog.MakeMockRedisCli(outputs)
 	w, outStr, outInt := pzsvc.GetMockResponseWriter()
 	getAsynchStatus(w, "aaaa")
 	if *outInt == http.StatusOK {
@@ -219,46 +102,46 @@ func TestGetAsynchStatus(t *testing.T) {
 }
 
 func TestGetAsynchResults(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	outputs := []string{
-		redisConvErrStr("Error: totally an error."),
-		redisConvStatus("Pending"),
-		redisConvStatus("Finished")}
-	redisCli = makeMockRedisCli(outputs)
+		catalog.RedisConvErrStr("Error: totally an error."),
+		catalog.RedisConvStatus("Pending"),
+		catalog.RedisConvStatus("Finished")}
+	redisCli = catalog.MakeMockRedisCli(outputs)
 	w, _, _ := pzsvc.GetMockResponseWriter()
 	getAsynchResults(w, "aaaa")
 	getAsynchResults(w, "aaaa")
 	getAsynchResults(w, "aaaa")
 }
 func TestRedisAddJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	outputs := []string{
-		redisConvErrStr("123")}
-	redisCli = makeMockRedisCli(outputs)
+		catalog.RedisConvErrStr("123")}
+	redisCli = catalog.MakeMockRedisCli(outputs)
 	redisAddJob("123", "Test")
 }
 func TestRedisTakeJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	outputs := []string{
-		redisConvString("123"),
-		redisConvStatus("Pending")}
-	redisCli = makeMockRedisCli(outputs)
+		catalog.RedisConvString("123"),
+		catalog.RedisConvStatus("Pending")}
+	redisCli = catalog.MakeMockRedisCli(outputs)
 	out1, out2, _ := redisTakeJob()
 	t.Log(out1)
 	t.Log(out2)
 }
 func TestRedisDoneJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	_ = redisDoneJob("123", "test")
 }
 
 func TestRedisErrorJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	redisErrorJob("123", "test")
 }
 
 func TestRedisClearJob(t *testing.T) {
-	mockConnCount = 0
+	catalog.SetMockConnCount(0)
 	_ = redisClearJob("123")
 }
 
